78952e2e20ffea577b90b45fe4cc940e784f8bce
Aleksandra Obukh <sem0609@mail.ru>
2023-01-10 15:47:45 +0300

    Array insertion and output

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+//Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+//лучше обойтись исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+Console.WriteLine("Введите строки массива через enter: ");
+
+string[] array = new string[4];
+
+CreateArray(array);
+Console.WriteLine();
+PrintArray(array);
+Console.WriteLine();
+
+void CreateArray(string[] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Console.ReadLine();
+    }
+}
+void PrintArray(string[] inArray)
+{
+    Console.Write("Введенный масив: [");
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        Console.Write($"{inArray [i]}, ");
+    }
+     Console.Write("]");
+}
